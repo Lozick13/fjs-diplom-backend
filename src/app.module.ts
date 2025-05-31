6922816bf9bc9b05126df12734f30bee09616b3e
Lozick <dlozichnyy2005@gmail.com>
2025-05-31 13:20:58 +0700

    init

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  controllers: [],
+  providers: [],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: `.${process.env.NODE_ENV}.env` }),
+  ],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+const start = async () => {
+  const PORT = process.env.PORT || 3000;
+  const app = await NestFactory.create(AppModule);
+
+  // swagger
+  const config = new DocumentBuilder()
+    .setTitle('Cайт-агрегатор просмотра и бронирования гостиниц')
+    .setDescription('Документация проекта')
+    .addTag('lozick13')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, document);
+
+  await app.listen(PORT, () => {
+    console.log(`Сервер запущен на порту: ${PORT}`);
+  });
+};
+
+void start();
