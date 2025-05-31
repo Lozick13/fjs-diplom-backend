9f57e341f3e8a456024634c1a6512951b5602fea
Lozick <dlozichnyy2005@gmail.com>
2025-05-31 13:22:41 +0700

    nest users init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
   controllers: [],
   providers: [],
   imports: [
     ConfigModule.forRoot({ envFilePath: `.${process.env.NODE_ENV}.env` }),
+    UsersModule,
   ],
 })
 export class AppModule {}
