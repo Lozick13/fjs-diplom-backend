ae99d30e809c1f507f29a2a1a64548995378cd6e
Lozick <dlozichnyy2005@gmail.com>
2025-06-15 17:08:53 +0700

    rename paths

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,28 +11,28 @@ export class UsersController {
 
   @ApiOperation({ summary: 'Создание пользователя' })
   @ApiResponse({ status: 200, type: User })
-  @Post()
+  @Post('/create')
   create(@Body() userDto: CreateUserDto) {
     return this.usersService.create(userDto);
   }
 
   @ApiOperation({ summary: 'Поиск пользователя по ID' })
   @ApiResponse({ status: 200, type: User })
-  @Get(':id')
+  @Get('/search/:id')
   findById(@Param('id') id: string) {
     return this.usersService.findById(id);
   }
 
   @ApiOperation({ summary: 'Поиск пользователя по email' })
   @ApiResponse({ status: 200, type: User })
-  @Get('email/:email')
+  @Get('search/email/:email')
   findByEmail(@Param('email') email: string) {
     return this.usersService.findByEmail(email);
   }
 
   @ApiOperation({ summary: 'Поиск пользователей по данным' })
   @ApiResponse({ status: 200, type: [User] })
-  @Get()
+  @Get('/search')
   @ApiQuery({ name: 'limit', required: false })
   @ApiQuery({ name: 'offset', required: false })
   @ApiQuery({ name: 'email', required: false })
